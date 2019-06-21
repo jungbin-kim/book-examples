d2fc9cdf0cdb9cb9fe0e663dcd3f7482967f8a15
Jungbin Kim <mail@jungbin.kim>
2019-06-22 07:43:42 +0900

    5.3 mergeMap 연산자 정리

--- part02/ch05/5-12.js
+++ part02/ch05/5-12.js
@@ -1,3 +1,11 @@
+
+/*
+5-9.js 와 비슷.
+mergeMap은 project 함수가 iterable 객체인지 검사(isIterable)하여 이터러블에서 이터레이터를 가져와서 next 함수를 호출해 구독.
+*/
+const { isArrayLike } = require('rxjs/util/isArrayLike');
+const { isIterable } = require('rxjs/util/isIterable');
+
 const { range } = require('rxjs');
 const { mergeMap } = require('rxjs/operators');
 
@@ -7,6 +15,7 @@ range(0, 3)
             const nextMap = new Map();
             nextMap.set('original', x);
             nextMap.set('plusOne', x + 1);
+            console.log(`isArrayLike(nextMap)=${isArrayLike(nextMap)}, isIterable(nextMap)=${isIterable(nextMap)}`)
             return nextMap;
         })
     )
--- part02/ch05/5-9.js
+++ part02/ch05/5-9.js
@@ -1,6 +1,18 @@
+/*
+mergeMap은 project 함수가 배열이나 유사 배열을 검사(isArrayLike)하여 순회하면서 구독한다.
+유사 배열을 구분하는 조건은 length 속성이 Number 타입인 객체
+*/
+
+const { isArrayLike } = require('rxjs/util/isArrayLike');
+
 const { range } = require('rxjs');
 const { mergeMap } = require('rxjs/operators');
 
 range(0, 3)
-    .pipe(mergeMap(x => [x + 1, x + 2, x + 3, x + 4]))
-    .subscribe(value => console.log(`current value: ${value}`));
+    .pipe(mergeMap(x => {
+        const array = [x + 1, x + 2, x + 3, x + 4]
+        console.log('isArrayLike(array) =', isArrayLike(array))
+        return array
+    })) // mergeMap이 배열을 반환
+    .subscribe(value => console.log(`current value: ${value}`)); // 하지만, value가 배열을 반환하지는 않는다.
+ // mergeMap을 사용하여 그 배열이 순회하면서 내부 item들이 하나씩 value에 나온다. 
