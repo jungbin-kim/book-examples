85bda0428da49af52d929e81fec188dcb85cd2aa
Jungbin Kim <mail@jungbin.kim>
2019-06-22 11:30:31 +0900

    5.6 scan 연산자 정리

--- part02/ch05/5-18.js
+++ part02/ch05/5-18.js
@@ -5,6 +5,9 @@ range(0, 3)
     .pipe(
         scan((accumulation, currentValue) => {
             console.log(
+                // 초기값 seed가 없으면 source에 가장 처음 값이 seed 값이 됨.
+                // Optional. Default is undefined. If no seed value is specified, the first item of the source is used as the seed. 
+                // from https://rxjs-dev.firebaseapp.com/api/operators/scan
                 `accumulation ${accumulation}, currentValue ${currentValue}`
             );
             return accumulation + currentValue;
--- part02/ch05/5-20.js
+++ part02/ch05/5-20.js
@@ -6,6 +6,7 @@ const source$ = interval(500).pipe(
     take(n),
     scan(
         (accumulation, currentValue) => {
+            // currentValue로는 interval(500)에서 나오는 정수값이 들어온다. 0,1,2...
             const tempA = accumulation.a;
             accumulation.a = accumulation.b;
             accumulation.b = tempA + accumulation.b;
@@ -15,8 +16,10 @@ const source$ = interval(500).pipe(
     ),
     pluck('a')
 );
-
+// 시작부터 500ms 사이로 7번 돌아 3000ms 후에는 종료된다.
 source$.subscribe(result => console.log(`result1 ${result}`));
+
+// 시작으로 3100ms 후에 새로 구독 시작. 결과를 보면 이전 옵저버블의 값이 남아있어 이어져서 실행된다. 이 문제를 해결하는 코드는 5-21, 5-22
 setTimeout(
     () =>
         source$.subscribe(result => console.log(`result2 ${result}`)),
