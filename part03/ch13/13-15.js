692e3442aa6ba7def1e5f4ae91f711b009fba53b
jungbin-kim <jungbin.kim@nhnent.com>
2019-07-11 19:33:46 +0900

    13.4 스케줄러에서 사용하는 연산자
    - subscribeOn 연산자
    - observeOn 연산자

--- part03/ch13/13-15.js
+++ part03/ch13/13-15.js
@@ -1,15 +1,25 @@
-const { Observable, asyncScheduler } = require('rxjs');
-const { subscribeOn } = require('rxjs/operators');
+const { Observable, asyncScheduler } = require('rxjs')
+const { subscribeOn } = require('rxjs/operators')
 
+// 스케줄러에 영향을 받는다.
 const source$ = Observable.create(observer => {
-    console.log('BEGIN source');
-    observer.next(1);
-    observer.next(2);
-    observer.next(3);
-    observer.complete();
-    console.log('END source');
-});
+  console.log('BEGIN source')
+  observer.next(1)
+  observer.next(2)
+  observer.next(3)
+  observer.complete()
+  console.log('END source')
+})
 
-console.log('before subscribe');
-source$.pipe(subscribeOn(asyncScheduler, 1000)).subscribe(x => console.log(x));
-console.log('after subscribe');
+console.log('before subscribe')
+source$.pipe(subscribeOn(asyncScheduler, 1000)).subscribe(x => console.log(x)) // 소스의 옵저버블 작업을 subscribeOn 에서 지정한 스케줄러로 실행. 1초 뒤에 구독됨
+console.log('after subscribe')
+/*
+before subscribe
+after subscribe 
+BEGIN source // 1초 후
+1
+2
+3
+END source
+*/
--- part03/ch13/13-18.js
+++ part03/ch13/13-18.js
@@ -1,15 +1,26 @@
-const { Observable, asyncScheduler } = require('rxjs');
-const { observeOn } = require('rxjs/operators');
+const { Observable, asyncScheduler } = require('rxjs')
+const { observeOn } = require('rxjs/operators')
 
 const source$ = Observable.create(observer => {
-    console.log('BEGIN source');
-    observer.next(1);
-    observer.next(2);
-    observer.next(3);
-    observer.complete();
-    console.log('END source');
-});
+  console.log('BEGIN source')
+  observer.next(1)
+  observer.next(2)
+  observer.next(3)
+  observer.complete()
+  console.log('END source')
+})
 
-console.log('before subscribe');
-source$.pipe(observeOn(asyncScheduler, 1000)).subscribe(x => console.log(x));
-console.log('after subscribe');
+console.log('before subscribe')
+// observeOn 연산자 다음으로 생성되는 옵저버블은 1초후 스케줄러를 이용해서 실행
+// subscribe 함수 안에 있는 next 함수 동작이 스케줄러의 영향을 받아 1부터 3까지 출력만 1초 후 비동기로 처리
+source$.pipe(observeOn(asyncScheduler, 1000)).subscribe(x => console.log(x))
+console.log('after subscribe')
+/*
+before subscribe
+BEGIN source
+END source
+after subscribe
+1
+2
+3
+*/
